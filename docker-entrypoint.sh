263a944c8a00630908a2c7b4340c610339be6104
Antoine Brun <antoine.brun@openmsa.co>
2025-08-18 17:40:10 +0200

    Update docker-entrypoint.sh
    fix test conditions on .git

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,7 +20,7 @@ then
 fi
 cd / || exit 1
 
-if [[ -f /opt/fmc_repository/OpenMSA_Workflow_Topology/.msa-dev ]]; then
+if [[ -d /opt/fmc_repository/OpenMSA_Workflow_Topology/.git ]]; then
 	log_info "👾 Skipping upgrade for fellow developer."
 	exit 0
 fi
